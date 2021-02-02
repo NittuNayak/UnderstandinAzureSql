8ee358a17d95cf72f4910bc6ff7b58e0f246d1e6
Mukesh Nayak <mukeshknayak@gmail.com>
2021-02-02 20:18:02 +0530

    Add project files.

---
+++ UnderstandinAzureSql/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderstandinAzureSql.AzureSQLDB;
+
+namespace UnderstandinAzureSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using(NayakEntities dbContext = new NayakEntities())
+                {
+                    List<Employee> employeeList = new List<Employee>()
+                    {
+                        new Employee(){ Name = "Mukesh", Age = 22 },
+                        new Employee(){ Name = "Manan", Age = 27 },
+                        new Employee(){ Name = "Juli", Age = 23}
+                    };
+                    
+
+                    dbContext.Employees.AddRange(employeeList);
+                    dbContext.SaveChanges();
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
